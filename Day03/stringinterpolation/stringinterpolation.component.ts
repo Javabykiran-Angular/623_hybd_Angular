ac0da37305b60cd79da954d3f30f5bda5ec304c1
The Kiran Academy <71135924+Javabykiran-Angular@users.noreply.github.com>
2024-06-21 12:34:11 +0530

    Day03

---
+++ Day03/stringinterpolation/stringinterpolation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stringinterpolation',
+  templateUrl: './stringinterpolation.component.html',
+  styleUrls: ['./stringinterpolation.component.css']
+})
+export class StringinterpolationComponent {
+    name:string='Sumit Raokhande';
+    imgUrl:string="../../assets/tka.jpeg";
+    
+}
