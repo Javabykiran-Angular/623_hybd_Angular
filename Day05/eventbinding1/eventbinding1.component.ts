8774d3de6a15a78feaee3cb08e85bbec2a936183
The Kiran Academy <71135924+Javabykiran-Angular@users.noreply.github.com>
2024-06-26 12:27:58 +0530

    Day05

---
+++ Day05/eventbinding1/eventbinding1.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-eventbinding1',
+  templateUrl: './eventbinding1.component.html',
+  styleUrls: ['./eventbinding1.component.css']
+})
+export class Eventbinding1Component {
+
+  count:number=0;
+  onClick(){
+
+    if(this.count==0){
+      console.log("Click Event Occur...");
+      this.count++;
+    }    
+  }
+
+  onImage(){
+    console.log("On Image Click...")
+  }
+
+  onSend(myname:string){
+    console.log(myname);
+  }
+
+  onSend1(myinputname1:any){
+    console.log(myinputname1);
+
+    myinputname1.style.background='green';
+    myinputname1.style.color="white";
+
+  }
+
+}
