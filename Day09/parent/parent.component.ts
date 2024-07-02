41a10c01e9964b926e15ef1efd3ea2d2626ea7ea
The Kiran Academy <71135924+Javabykiran-Angular@users.noreply.github.com>
2024-07-02 12:21:10 +0530

    Day09

---
+++ Day09/parent/parent.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-parent',
+  templateUrl: './parent.component.html',
+  styleUrls: ['./parent.component.css']
+})
+export class ParentComponent {
+
+  jsonData={
+    id:9,
+    fname:'Sumit',
+    lname:'Raokhande'
+  }
+  
+
+}
