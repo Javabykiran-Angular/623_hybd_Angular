e726e1adfaf351b6f2ff1a83233caa3aa8671663
The Kiran Academy <71135924+Javabykiran-Angular@users.noreply.github.com>
2024-07-08 12:16:46 +0530

    Day13

---
+++ Day13/app.module.ts
@@ -0,0 +1,65 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { StudentComponent } from './student/student.component';
+import { StringinterpolationComponent } from './stringinterpolation/stringinterpolation.component';
+import { PropertybindingComponent } from './propertybinding/propertybinding.component';
+import { StylebindingComponent } from './stylebinding/stylebinding.component';
+import { ClassbindingComponent } from './classbinding/classbinding.component';
+import { Eventbinding1Component } from './eventbinding1/eventbinding1.component';
+import { Eventbinding2Component } from './eventbinding2/eventbinding2.component';
+import { FormsModule } from '@angular/forms';
+import { Eventbinding3Component } from './eventbinding3/eventbinding3.component';
+import { IfdirectiveComponent } from './ifdirective/ifdirective.component';
+import { FordirectiveComponent } from './fordirective/fordirective.component';
+import { SwitchdirectiveComponent } from './switchdirective/switchdirective.component';
+import { ParentComponent } from './parent/parent.component';
+import { Child1Component } from './child1/child1.component';
+import { Child2Component } from './child2/child2.component';
+import { Parent2Component } from './parent2/parent2.component';
+import { InbuiltpipeComponent } from './inbuiltpipe/inbuiltpipe.component';
+import { CustomPipeComponent } from './custom-pipe/custom-pipe.component';
+import { SummaryPipe } from './summary.pipe';
+import { CourseComponent } from './course/course.component';
+import { PostComponent } from './post/post.component';
+
+import {HttpClientModule} from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmployeeComponent,
+    StudentComponent,
+    StringinterpolationComponent,
+    PropertybindingComponent,
+    StylebindingComponent,
+    ClassbindingComponent,
+    Eventbinding1Component,
+    Eventbinding2Component,
+    Eventbinding3Component,
+    IfdirectiveComponent,
+    FordirectiveComponent,
+    SwitchdirectiveComponent,
+    ParentComponent,
+    Child1Component,
+    Child2Component,
+    Parent2Component,
+    InbuiltpipeComponent,
+    CustomPipeComponent,
+    SummaryPipe,
+    CourseComponent,
+    PostComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
